e2c869b8b4f613025b7d992a823264aa9a160344
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-03-21 16:39:35 +0530

    error handling

--- day-2/greetings/greetings.go
+++ day-2/greetings/greetings.go
@@ -9,7 +9,7 @@ import (
 func Hello(name string) (string,error) {
 
 	if name == "" {
-		return "", errors.New("empty name")
+		return "", errors.New("Please provide a name")
 	}
 
 	message := fmt.Sprintf("Hello %v. How are you?",name)
--- day-2/hello/hello.go
+++ day-2/hello/hello.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"greetings"
+	"log"
 )
 
 func main(){
-	message := greetings.Hello("Harsha")
 
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("Harsha")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }

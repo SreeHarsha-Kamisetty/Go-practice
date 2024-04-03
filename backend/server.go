d645d3df707f4d67c121402085db14492e3f10a3
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-04-03 18:01:07 +0530

    handling different routes

--- backend/server.go
+++ backend/server.go
@@ -9,8 +9,19 @@ func homeRoute(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Home route")
 }
 
+func aboutRoute(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"About route")
+}
+
+func contactRoute(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Contact route")
+}
 func main(){
 	http.HandleFunc("/",homeRoute)
+	http.HandleFunc("/about",aboutRoute)
+	http.HandleFunc("/contact",contactRoute)
+	
+	
 	fmt.Println("Server is running at http://localhost:8080")
 	http.ListenAndServe(":8080",nil)
 }
